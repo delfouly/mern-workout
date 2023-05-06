c0fc2fcaaca21bfee524daa2a077c0b1f3fd2259
Donia El Fouly <donia.elfouly@gmail.com>
2023-05-06 11:32:11 +0300

    add server auth

--- frontend/src/hooks/useWorkoutsContext.tsx
+++ frontend/src/hooks/useWorkoutsContext.tsx
@@ -3,5 +3,10 @@ import { WorkoutsContext } from "../context/WorkoutsContext";
 
 export function useWorkoutsContext() {
   const context = React.useContext(WorkoutsContext);
+  if (!context) {
+    throw Error(
+      "useWorkoutsContext must be used inside WorkoutsContextProvider"
+    );
+  }
   return context;
 }
